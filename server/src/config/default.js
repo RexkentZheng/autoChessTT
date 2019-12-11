2ff38d9655bb49d752898262ab74ba797bb69819
RexkentZheng <331749897@qq.com>
2019-12-11 14:24:22 +0800

    refactor: 初始化项目

---
+++ server/src/config/default.js
@@ -0,0 +1,33 @@
+const config = {
+  "db": {
+    "database": "dzvpn-subserver",
+    "username": "root",
+    "password": "root",
+    "options": {
+      "dialect": "mysql",
+      "host": "127.0.0.1",
+      "port": 3306,
+      "operatorsAliases": false,
+      "define": {
+        "underscored": false,
+        "freezeTableName": true,
+        "charset": "utf8",
+        "dialectOptions": {
+          "collate": "utf8_general_ci"
+        },
+        "engine": "InnoDB"
+      },
+      "pool": {
+        "min": 2,
+        "max": 10,
+        "idle": 300000
+      }
+    }
+  },
+  "server": {
+    "port": 9929
+  },
+  "mailUrl": "http://127.0.0.1:8987/register",
+};
+
+module.exports = config;
